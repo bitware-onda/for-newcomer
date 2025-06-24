0bd1c456a8d7d857ff6d616697a48498e99de533
Genki Onda <g.onda@bit-ware.co.jp>
2025-06-24 15:11:49 +0900

    pythonコーディングの編集 修正完了いたしました。

--- bug_cafe/main.py
+++ bug_cafe/main.py
@@ -16,14 +16,14 @@ def take_order(menus, order_number):
     for i, menu in enumerate(menus):
         print(f"({i + 1}) {menu['name']}: {menu['price']}円")
     print(f"> {order_number}")
-    print(f"{menus[order_number]['name']}（{menus[order_number]['price']}円）ですね。")
-    return order_number
+    print(f"{menus[order_number-1]['name']}（{menus[order_number-1]['price']}円）ですね。")
+    return order_number-1
 
 print("ご注文を番号でどうぞ。")
 order1 = take_order(DRINKS, 2)
 print("フードメニューはいかがですか?")
 order2 = take_order(FOODS, 1)
 
-total = FOODS[order1]["price"] + DRINKS[order2]["price"]
+total = int(DRINKS[order1]["price"]) + int(FOODS[order2]["price"])
 
 print(f"お会計は{total}円になります。ありがとうございました！")
